2819613a2e037dbcbed46890be25c02925c32207
geno0707 <geno0707@rocketmail.com>
2026-07-27 02:10:32 -0400

    Update poke-database.ts

--- poke-database.ts
+++ poke-database.ts
@@ -1,1 +1,13 @@
 import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+//ROUTE
+
+app.listen(PORT, () => console.log(`listening on ${PORT}`)); // starts the server and runs the callback as soon as its up.
+//when the server is ready run this!
+
+
+
+
