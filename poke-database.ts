88cbdd104e2b71105706ab3d43fa334d48bd8c6c
geno0707 <geno0707@rocketmail.com>
2026-07-28 22:19:01 -0400

    rdm 3

--- poke-database.ts
+++ poke-database.ts
@@ -45,7 +45,33 @@ export interface PokemonStruct {
         console.error(err);
         return res.status(502).json({ error: "upstream service unavailable" }) // bad gateway
     }
-})
+})*/
+
+app.get("/pokemon/random", async (req, res) => {
+    try{
+        let rdm = [0, 0, 0]
+        for (let i = 0; i < rdm.length; i++){
+        rdm.push(Math.floor(Math.random() * 1025) + 1);
+        }
+
+        const team = await Promise.all([
+            fetchPokemon(rdm[0].toString()),
+            fetchPokemon(rdm[1].toString()),
+            fetchPokemon(rdm[2].toString())
+        ]);
+
+        if (team.some(x => x === null)){
+            return res.status(404).json({ error: "One or more pokemon not found, this probably impossible to reach"});
+        }
+        return res.json(team); 
+    }catch (err){
+        console.error(err);
+        return res.status(502).json({error: " unvailable service. "});
+    }
+    
+} )
+
+
 
 app.get("/pokemon/:name", async (req, res) => {
     try {
@@ -60,7 +86,7 @@ app.get("/pokemon/:name", async (req, res) => {
             return res.status(502).json({ error: "upstream service unavailable" })
     }
 
-});*/
+});
 
 app.get("/team", async (req, res) => {
     try{
